93db832989a88e4296b90aa64102dba82de73b19
philipszdavido <kurtwanger40@gmail.com>
2021-10-06 12:30:50 +0100

    Made some changes

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -1,53 +1,64 @@
 import * as React from "react";
-import { RouteProp } from '@react-navigation/core';
-import { Dialogs } from '@nativescript/core';
+import { RouteProp } from "@react-navigation/core";
+import { Dialogs } from "@nativescript/core";
 import { FrameNavigationProp } from "react-nativescript-navigation";
 import { StyleSheet } from "react-nativescript";
 import { MainStackParamList } from "./NavigationParamList";
+import {
+  WebViewExtBase,
+  WebViewExt
+} from "@nota/nativescript-webview-ext/webview-ext";
 
 type HomeScreenProps = {
-    route: RouteProp<MainStackParamList, "Home">,
-    navigation: FrameNavigationProp<MainStackParamList, "Home">,
-}
+  route: RouteProp<MainStackParamList, "Home">;
+  navigation: FrameNavigationProp<MainStackParamList, "Home">;
+};
+
+const htmlString = `
+<html>
+  <head>
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="https://api.tomtom.com/maps-sdk-for-web/cdn/5.x/5.69.1/maps/maps.css"
+    />
+    <script src="https://api.tomtom.com/maps-sdk-for-web/cdn/5.x/5.69.1/maps/maps-web.min.js"></script>
+  </head>
+  <body style="width: 100%; height: 100%; margin: 0; padding: 0">
+    <div
+      id="map"
+      style="width: 100%; height: 100%; background-color: orangered"
+    ></div>
+    <script>
+      var map = tt.map({
+        key: 'IGZBF1VBk0AjcA8NnrGp9RcG1AeYDsuC',
+        container: 'map',
+        style: 'tomtom://vector/1/basic-main',
+      });
+    </script>
+  </body>
+</html>
+`;
 
 export function HomeScreen({ navigation }: HomeScreenProps) {
-    return (
-        <flexboxLayout style={styles.container}>
-            <label
-                className="fas"
-                style={styles.text}
-            >
-                &#xf135; Hello World!
-            </label>
-            <button
-                style={styles.button}
-                onTap={() => Dialogs.alert("Tapped!")}
-            >
-                Tap me for an alert
-            </button>
-            <button
-                style={styles.button}
-                onTap={() => navigation.navigate('Secondary')}
-            >
-                Go to next screen
-            </button>
-        </flexboxLayout>
-    );
+  return (
+    <flexboxLayout style={styles.container}>
+      <webView src={htmlString} />
+    </flexboxLayout>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        height: "100%",
-        flexDirection: "column",
-        justifyContent: "center",
-    },
-    text: {
-        textAlignment: "center",
-        fontSize: 24,
-        color: "black",
-    },
-    button: {
-        fontSize: 24,
-        color: "#2e6ddf",
-    },
+  container: {
+    height: "100%"
+  },
+  text: {
+    textAlignment: "center",
+    fontSize: 24,
+    color: "black"
+  },
+  button: {
+    fontSize: 24,
+    color: "#2e6ddf"
+  }
 });
